f2f0dd2d5018868eecf420511e1c9e4f1169a65f
ktpeng <tkentson@gmail.com>
2024-04-08 22:19:45 -0500

    Modifying script to preserve original modify date on newly edited images

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 import shutil
 from PIL import Image, ExifTags, ImageDraw, ImageFont
 
+#this script is for time stamping sony cybershot dsc wx150 images of default settings
+
 #convert date to return list of [date, time]
 def militaryTimeConvert(date):
     if int(date[:2]) >= 12:
@@ -29,7 +31,7 @@ def createDatestampedDir():
         print('directory cleared')
 
 #editing the images to apply date and time in bottom right corner
-def modifyImages(imgDateDict):
+def modifyImages(imgDateDict, origTimeDict):
     cnt = 0
     tot = len(os.listdir('./datestamped'))
     for file in os.listdir('./datestamped'):
@@ -54,6 +56,9 @@ def modifyImages(imgDateDict):
         cnt += 1
         print('[%d / %d]' %(cnt, tot))
 
+        #after editing images, change the modify time using the orig img time dictionary
+        os.utime(path,(origTimeDict[file].st_atime,origTimeDict[file].st_mtime))
+
 def main():
     createDatestampedDir()
 
@@ -62,16 +67,24 @@ def main():
     cnt,prcnt = 0, 0
     imgDateDict = {}
     print('\nCopying files over to new directory to not modify original images')
+    origTimeDict = {}
     for i in entries:
         path = dirPath+"\\"+i
+        #copying images to newly created folder
         shutil.copy(path, './datestamped')
+
+        #getting time data to put on the image using pillow library
         m_time = os.path.getmtime(path)
         dt_m = datetime.datetime.fromtimestamp(m_time)
         time = militaryTimeConvert(dt_m.strftime("%H:%M"))
         imgDateDict[i] = [dt_m.strftime("%m/%d/%Y"), time]
 
+        #inserting elements into dictionary conisting of [imgName] = [origImgMetaData]
+        st = os.stat(path)
+        origTimeDict[i] = st
+
     print('All files copied over, preparing to edit')
-    modifyImages(imgDateDict)
+    modifyImages(imgDateDict, origTimeDict)
     print('All images completed!')
 
 
